ada02522dca3ae74e8fb203a79a54719e44af8ca
Niji Ano <fab-bertheol@hotmail.fr>
2022-09-08 18:23:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,57 @@
-#!/usr/bin/env python3
-import setuptools
-import versioneer
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-with open("requirements.txt", "r") as fh:
-    requirements = [line.strip() for line in fh]
+'''
+Created by Anoniji
+Library made available under the terms of the license
+Creative Commons Zero v1.0 Universal
+https://creativecommons.org/publicdomain/zero/1.0/
+'''
 
-setuptools.setup(
-    name="tama-voices",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Niji Ano",
-    author_email="github@anoniji.dev",
-    description="Tools for creating voicepacks for Tama",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Creative Commons Zero v1.0 Universal",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.5',
-    install_requires=requirements,
+import sys
+import os
+from cx_Freeze import setup, Executable
+import site
+import shutil
+
+VERSION = '0.1.0'
+cible = Executable(script='main.py', base='Console', targetName="voicemaker.exe")
+
+setup(
+    name='Data Colors',
+    version=VERSION,
+    author='Anoniji',
+    options={
+        'build_exe': {
+            'path': sys.path,
+            'includes': [
+                'logger',
+            ],
+            'excludes': [],
+            'packages': [],
+            'optimize': 1,
+            'silent': True,
+            'zip_include_packages': '*',
+            'zip_exclude_packages': '',
+            'include_msvcr': True,
+            'build_exe': './build/',
+        },
+    },
+    executables=[cible],
 )
+
+
+if not os.path.isdir('.\\.build\\lib\\_soundfile_data\\'):
+    os.mkdir('.\\.build\\lib\\_soundfile_data\\')
+
+shutil.copyfile(
+    site.getsitepackages()[-1] + '\\_soundfile_data\\libsndfile64bit.dll',
+    '.\\.build\\lib\\_soundfile_data\\libsndfile64bit.dll'
+)
+
+shutil.copytree(
+    '.\\dictionaries',
+    '.\\.build\\dictionaries'
+)
+
+print(site.getsitepackages()[-1] + '\\_soundfile_data\\libsndfile64bit.dll')
